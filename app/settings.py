1c9479e6f35f5fb2039f4fb06fc9ad29c40616bd
Gabriel <o_gabrieltorres@outlook.com>
2025-08-25 14:47:22 -0300

    inserida as informações de conexão ao BD

--- app/settings.py
+++ app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crud_simples',
+        'USER': 'crud_simples_user',
+        'PASSWORD': '123qwe',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
